2d1d25e1225e35c58aebd51b8b83b16240ec07f4
John DoeOlesya <olesya.gerasimova@me.com>
2023-03-15 12:59:44 +0500

    1

---
+++ mypackage/calc.py
@@ -0,0 +1,25 @@
+importmypackage.calc as calc
+__all__ = ['add','diff']
+
+# Модуль вычислений
+
+def add(x, y):
+    return x + y
+
+def diff(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+def divint(x,y):
+    return x//y
+
+def func():
+    pass
+
+if __name__ == "__main__":
+    pass
